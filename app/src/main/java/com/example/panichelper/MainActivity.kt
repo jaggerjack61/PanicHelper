aa02b65f227ee6024d4636ecc1f2d685ff41e14e
Samuel Jarai <jarai.samuel@gmail.com>
2022-02-11 12:08:57 +0200

    add settings activity and removed the actionbar from all activities.

--- app/src/main/java/com/example/panichelper/MainActivity.kt
+++ app/src/main/java/com/example/panichelper/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.panichelper
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val getHelpBtn=findViewById<Button>(R.id.button)
+        getHelpBtn.setOnClickListener {
+            getHelp()
+        }
+
+
+
+        val settingsBtn=findViewById<Button>(R.id.button3)
+        settingsBtn.setOnClickListener {
+            val intent= Intent(this,SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+    private fun getHelp(){
+        val sharedPreferences = getSharedPreferences("savedSettings", Context.MODE_PRIVATE)
+        
+
+
     }
 }
---
+++ app/src/main/java/com/example/panichelper/SettingsActivity.kt
@@ -0,0 +1,56 @@
+package com.example.panichelper
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        loadSettings()
+        val saveSettingsBtn=findViewById<Button>(R.id.button)
+        saveSettingsBtn.setOnClickListener {
+            saveSettings()
+        }
+
+
+
+    }
+    private fun saveSettings(){
+        val phoneNumber=findViewById<EditText>(R.id.editTxt).text.toString()
+        val email=findViewById<EditText>(R.id.editTxt2).text.toString()
+        val customText=findViewById<EditText>(R.id.editTxt3).text.toString()
+
+        val sharedPreferences = getSharedPreferences("savedSettings", Context.MODE_PRIVATE)
+        val editor=sharedPreferences.edit()
+        editor.apply{
+            putString("phoneNumber",phoneNumber)
+            putString("email",email)
+            putString("customText",customText)
+
+        }.apply()
+
+        Toast.makeText(this,"Settings have been saved",Toast.LENGTH_SHORT).show()
+
+
+    }
+
+    private fun loadSettings(){
+        val sharedPreferences = getSharedPreferences("savedSettings", Context.MODE_PRIVATE)
+        val phoneNumber=sharedPreferences.getString("phoneNumber",null)
+        val email=sharedPreferences.getString("email",null)
+        val customText=sharedPreferences.getString("customText",null)
+        val phoneNumberTxt=findViewById<EditText>(R.id.editTxt)
+        val emailTxt=findViewById<EditText>(R.id.editTxt2)
+        val customTextTxt=findViewById<EditText>(R.id.editTxt3)
+        phoneNumberTxt.setText(phoneNumber)
+        emailTxt.setText(email)
+        customTextTxt.setText(customText)
+
+    }
+}
